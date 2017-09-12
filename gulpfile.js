eb1653e72ee52478a6ed6fbaed7de93303c50b72
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2017-09-12 12:41:04 +0700

    adaptive on NODE_ENV

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp       = require('gulp');
 var browserify = require('gulp-browserify');
 var concat     = require('gulp-concat');
 
+var prod = process.env.NODE_ENV == 'production';
+
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
@@ -13,20 +15,24 @@ gulp.task('default', gulp.series(
 // Compile Sass into CSS
 // In production, the CSS is compressed
 function sass() {
-  return gulp.src('src/scss/app.scss')
+  var g = gulp.src('src/scss/app.scss')
     .pipe($.sass()
-      .on('error', $.sass.logError))
-    .pipe($.cssnano())
-    .pipe(gulp.dest('dist/css'));
+      .on('error', $.sass.logError));
+  if (prod) {
+    g.pipe($.cssnano());
+  }
+  return g.pipe(gulp.dest('dist/css'));
 }
 
 // Compile react in coffeescript into javascript
 function compileReact() {
-  return gulp.src('./src/react/index.coffee', { read: false })
+  var g = gulp.src('./src/react/index.coffee', { read: false })
     .pipe(browserify({ transform: ['coffeeify'], extensions: ['.coffee'] }))
-    .pipe(concat('app2.js'))
-    .pipe($.uglify().on('error', function(e) { console.log(e); }))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(concat('app2.js'));
+  if (prod) {
+    g.pipe($.uglify().on('error', function(e) { console.log(e); }));
+  }
+  return g.pipe(gulp.dest('dist/js'));
 }
 
 // Watch for changes to static assets, pages, Sass, JavaScript, and CoffeeScript
