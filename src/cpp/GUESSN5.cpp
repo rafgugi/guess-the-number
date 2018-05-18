2a531ba5cf868d4ad84707c4a47fc872be1d79d5
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2018-05-18 13:13:19 +0700

    bab 4.1, using algorithm. tipisin repetisi kode

--- src/cpp/GUESSN5.cpp
+++ src/cpp/GUESSN5.cpp
@@ -2,34 +2,25 @@
 #include <math.h>
 
 int main(int argc, char const *argv[]) {
-    int t; // test cases
-    int n, w, m; // a test case
-    int q; // query count
-    int at_count; // always truth repeater (2w + 1)
-    int ceilog2; // ceil(log2(n))
+    int t, n, w, m; // a test case
+    int q, qe, qb;
 
     char s[4097];
-    char flag = 0;
 
     scanf("%d", &t);
     while (t--) {
         scanf("%d%d%d", &n, &w, &m);
 
-        at_count = 2 * w + 1;
-        ceilog2 = ceil(log2(n));
-        q = at_count * ceilog2;
+        qe = 2 * w + 1; // repeater
+        qb = ceil(log2(n)); // closest power of 2
+        q = qe * qb; // query count
         printf("%d\n", q);
-        for (int i = 0, pow2 = 1; i < ceilog2; ++i, pow2 *= 2) {
-            flag = 1;
-            int j;
-            for (j = 0; j < n; ++j) {
-                if (j % pow2 == 0) {
-                    flag = !flag; // reverse flag
-                }
-                s[j] = flag ? '1' : '0';
+        for (int i = 0, pow2 = 1; i < qb; ++i, pow2 <<= 1) {
+            for (int j = 0; j < n; ++j) {
+                s[j] = j & pow2 ? '1' : '0';
             }
-            s[j] = 0;
-            for (int j = 0; j < at_count; ++j) {
+            s[n] = 0;
+            for (int j = 0; j < qe; ++j) {
                 printf("%s\n", s);
             }
         }
